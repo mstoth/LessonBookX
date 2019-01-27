0ec1a0d529925c69b3eae83d206e4e47bd786254
Michael Toth <michael@virtualpianist.com>
2019-01-27 03:55:15 -0500

    Initial Commit

---
+++ LessonBookX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LessonBookX
+//
+//  Created by Michael Toth on 1/27/19.
+//  Copyright © 2019 Michael Toth. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ LessonBookXTests/LessonBookXTests.swift
@@ -0,0 +1,34 @@
+//
+//  LessonBookXTests.swift
+//  LessonBookXTests
+//
+//  Created by Michael Toth on 1/27/19.
+//  Copyright © 2019 Michael Toth. All rights reserved.
+//
+
+import XCTest
+@testable import LessonBookX
+
+class LessonBookXTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
